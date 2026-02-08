2a969eff20195740bd61aee4cb4f2681c7c70c0b
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-02-08 11:08:19 +0800

    Run app from main

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+import yaml
+
+from src.ingestion.create_schema import create_star_schema, create_staging_table
+from src.ingestion.load_staging import load_staging_table
+
+
 def main():
-    print("Hello from fraud-detection-pipeline!")
+    create_star_schema()
+    create_staging_table()
+
+    if len(sys.argv) > 1:
+        csv_file_path = sys.argv[1]
+    else:
+        config = yaml.safe_load(open("config/config.yaml"))
+        csv_file_path = config.get("data", {}).get("raw_csv", "data/raw/paysim1.csv")
+
+    load_staging_table(csv_file_path)
 
 
 if __name__ == "__main__":
